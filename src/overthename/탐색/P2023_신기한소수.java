2971f3cafd4cd3ecdd40d2cc4afaf18efe895b8a
sia7997 <sia7997@naver.com>
2022-07-16 02:43:52 +0900

    [code] 2022-07-15

---
+++ src/overthename/탐색/P2023_신기한소수.java
@@ -0,0 +1,39 @@
+package overthename.탐색;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.concurrent.ThreadPoolExecutor;
+
+public class P2023_신기한소수 {
+	static int N;
+	public static  void main(String[] args) throws IOException {
+	/*	Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+
+		//일의 자리 소수는 2,3,4,5
+		DFS(2,1);
+		DFS(3,1);
+		DFS(5,1);
+		DFS(7,1);
+	}
+
+	static void DFS(int num, int jarisu){
+		if( jarisu == N){
+			if( isPrime(number)){
+				System.out.println(number);
+			}
+		}
+		for( int i=1; i<10; i++){
+			if(i%2==0){
+				continue;
+			}
+			if(isPrime(number*10+i)){
+
+			}
+		}
+	}*/}
+}
