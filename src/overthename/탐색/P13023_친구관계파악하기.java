d78db088fb27e7d18a69747b27abe5383fe76dca
sia7997 <sia7997@naver.com>
2022-07-17 14:26:41 +0900

    [code] 2022-07-17

---
+++ src/overthename/탐색/P13023_친구관계파악하기.java
@@ -0,0 +1,33 @@
+package overthename.탐색;
+
+import java.io.IOException;
+
+public class P13023_친구관계파악하기 {
+	static int N;
+	public static  void main(String[] args) throws IOException {
+	/*	Scanner sc = new Scanner(System.in);
+		N = sc.nextInt();
+
+		//일의 자리 소수는 2,3,4,5
+		DFS(2,1);
+		DFS(3,1);
+		DFS(5,1);
+		DFS(7,1);
+	}
+
+	static void DFS(int num, int jarisu){
+		if( jarisu == N){
+			if( isPrime(number)){
+				System.out.println(number);
+			}
+		}
+		for( int i=1; i<10; i++){
+			if(i%2==0){
+				continue;
+			}
+			if(isPrime(number*10+i)){
+
+			}
+		}
+	}*/}
+}
--- src/overthename/탐색/P2023_신기한소수.java
+++ src/overthename/탐색/P2023_신기한소수.java
@@ -10,30 +10,42 @@ import java.util.concurrent.ThreadPoolExecutor;
 
 public class P2023_신기한소수 {
 	static int N;
-	public static  void main(String[] args) throws IOException {
-	/*	Scanner sc = new Scanner(System.in);
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(System.in);
 		N = sc.nextInt();
 
 		//일의 자리 소수는 2,3,4,5
-		DFS(2,1);
-		DFS(3,1);
-		DFS(5,1);
-		DFS(7,1);
+		DFS(2, 1);
+		DFS(3, 1);
+		DFS(5, 1);
+		DFS(7, 1);
 	}
 
-	static void DFS(int num, int jarisu){
-		if( jarisu == N){
-			if( isPrime(number)){
-				System.out.println(number);
+	static void DFS(int num, int jarisu) {
+		if (jarisu == N) {
+			if (isPrime(num)) {
+				System.out.println(num);
 			}
 		}
-		for( int i=1; i<10; i++){
-			if(i%2==0){
+		for (int i = 1; i < 10; i++) {
+			if (i % 2 == 0) {
 				continue;
 			}
-			if(isPrime(number*10+i)){
-
+			if (isPrime(num * 10 + i)) {
+				DFS(num * 10 + i, jarisu + 1);
 			}
 		}
-	}*/}
+	}
+
+	static boolean isPrime(int num) {
+		for (int i = 2; i <= num / 2; i++) {
+			if (num % i == 0) {
+				return false; }
+			}
+		return true;
+	}
+
+
 }
+
