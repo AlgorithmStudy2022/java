800596c151bfa3619e093829978168c4b5f0b746
sia7997 <sia7997@naver.com>
2022-07-18 02:36:01 +0900

    [code] 2022-07-17

--- src/overthename/탐색/P13023_친구관계파악하기.java
+++ src/overthename/탐색/P13023_친구관계파악하기.java
@@ -1,33 +1,54 @@
 package overthename.탐색;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class P13023_친구관계파악하기 {
-	static int N;
+	static boolean visited[];
+	static ArrayList<Integer>[] A;
+	static boolean arrive;
+
 	public static  void main(String[] args) throws IOException {
-	/*	Scanner sc = new Scanner(System.in);
-		N = sc.nextInt();
+		int N;
+		int M;
+		arrive = false;
+		Scanner scan = new Scanner(System.in);
+		N=scan.nextInt(); //노드개수
+		M=scan.nextInt(); //에지개수
+		A= new ArrayList[N];
+		visited = new boolean[N];
+		for(int i=0; i<N;i++){
+			A[i] = new ArrayList<Integer>();
+		}
+		for(int i=0; i<M; i++){
+			int S=scan.nextInt();
+			int E = scan.nextInt();
+			A[S].add(E);
+			A[E].add(S);
+		}
+		for(int i=0; i<N; i++){
+			DFS(i,1);
+			if(arrive)
+				break;
+		}
+		if(arrive)
+			System.out.println("1");
+		else
+			System.out.println("0");
 
-		//일의 자리 소수는 2,3,4,5
-		DFS(2,1);
-		DFS(3,1);
-		DFS(5,1);
-		DFS(7,1);
 	}
-
-	static void DFS(int num, int jarisu){
-		if( jarisu == N){
-			if( isPrime(number)){
-				System.out.println(number);
+	static void DFS(int v,int depth){
+		if(depth==5 || arrive){
+			arrive = true;
+		}
+		visited[v]=true;
+		for(int i : A[v]){
+			if(visited[i]==false){
+				DFS(v,depth+1);
 			}
 		}
-		for( int i=1; i<10; i++){
-			if(i%2==0){
-				continue;
-			}
-			if(isPrime(number*10+i)){
+		visited[v]=false;
 
-			}
-		}
-	}*/}
+	}
 }
